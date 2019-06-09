65f58a73f6b6b5afd60eeb8c2c178754e6ff2eb2
James Ladd <ladd.james@gmail.com>
2019-06-10 09:32:27 +1000

    Removed provisioning of geckodriver in favour of NPM based driver. Added chrome as browser.

--- features/support/driver.js
+++ features/support/driver.js
@@ -1,2 +1,3 @@
+const selenium_driver = process.env.SELENIUM_DRIVER || 'chrome';
 export const driver = require("selenium-webdriver");
-export const browser = new driver.Builder().forBrowser("firefox").build();
+export const browser = new driver.Builder().forBrowser(selenium_driver).build();
